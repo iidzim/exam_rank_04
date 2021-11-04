4c00bf997ad750f53ad6e647936427beae582590
Ikram Idzim <iidzim@e2r5p2.1337.ma>
2021-11-04 12:45:47 +0100

    pipe still doesnt work

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
-int print_error(char *msg)
+int print_msg(char *s)
 {
 	int i;
 
 	i = 0;
-	while (msg[i])
+	while (s[i])
 		i++;
-	write(2, msg, i);
+	write(2, s, i);
 	return (1);
 }
 
@@ -18,42 +18,39 @@ int error_fatal(char **cmd)
 {
 	if (cmd)
 		free(cmd);
-	exit (print_error("error: fatal\n"));
+	exit(print_msg("error: fata;\n"));
 }
 
-int cmd_size(char **argv, char *s)
+int cmd_size(char **cmd, char s)
 {
 	int i;
 
-	if (!argv)
+	if (!cmd)
 		return (0);
 	i = 0;
-	while (argv[i])
+	while (cmd[i])
 	{
-		if (!strcmp(argv[i], s))
+		if (!strcmp(cmd[i], s))
 			return (i);
 		i++;
 	}
-	// fprintf(stderr, "--------%d\n", i);
 	return (i);
 }
 
 char **parse(char **argv, int *i)
 {
+	int j;
 	int size;
 	char **cmd;
-	int k;
 
-	size = cmd_size(&argv[*i], ";");
+	size = cmd_size(argv, ";");
 	cmd = (char **)malloc(sizeof(char *) * (size + 1));
 	if (!cmd)
 		error_fatal(cmd);
-	k = -1;
-	while (++k < size){
-		cmd[k] = argv[k + *i];
-		// printf("-> %s\n", cmd[k]);
-	}
-	cmd[k] = NULL;
+	j = -1;
+	while (++j < size)
+		cmd[j] = argv[j + *i];
+	cmd[j] = NULL;
 	*i += size;
 	return (cmd);
 }
@@ -66,14 +63,13 @@ int cd_builtin(char **cmd)
 	while (cmd[i])
 		i++;
 	if (i != 2)
-		print_error("error: cd: bad arguments\n");
+		print_msg("error: cd: bad arguments\n");
 	if (chdir(cmd[1]) < 0)
 	{
-		print_error("error: cd: cannot change directory to ");
-		print_error(cmd[1]);
-		print_error("\n");
+		print_msg("error: cd: cannot change directory to ");
+		print_msg(cmd[1]);
+		print_msg("\n");
 	}
-	return (0);
 }
 
 char **next_pipe(char **cmd)
@@ -82,7 +78,6 @@ char **next_pipe(char **cmd)
 
 	if (!cmd)
 		return (NULL);
-	i = 0;
 	while (cmd[i])
 	{
 		if (!strcmp(cmd[i], "|"))
@@ -102,9 +97,9 @@ int exec_cmd(char **cmd, char **env, char **tmp)
 	{
 		if (execve(cmd[0], cmd, env) < 0)
 		{
-			print_error("error: cannot execute ");
-			print_error(cmd[0]);
-			print_error("\n");
+			print_msg("error: cannot execute ");
+			print_msg(cmd[0]);
+			print_msg("\n");
 		}
 	}
 	waitpid(0, NULL, 0);
@@ -117,60 +112,51 @@ int child_process(char **cmd, char **tmp, int pipefd[2], int in, char **env)
 		error_fatal(cmd);
 	if (next_pipe(tmp))
 		close(pipefd[0]);
-	if (next_pipe(tmp) && dup2(pipefd[1], 1) < 0)
-		error_fatal(cmd);
-	// fprintf(stderr, "======%d\n", cmd_size(cmd, "|"));
-	tmp[cmd_size(cmd, "|")] = NULL;
-	// fprintf(stderr, "*/*%s\n", tmp[1]);
-	// fprintf(stderr, "%s\n", next_pipe(cmd)[0]);
+	if (next_pipe(tmp) && dup2(pipefd[1], 1) < 0)	
+		error_fatal(tmp);
+	tmp[cmd_size(tmp, "|")] = NULL;
 	if (execve(tmp[0], tmp, env) < 0)
 	{
-		print_error("error: cannot execute ");
-		print_error(tmp[0]);
-		print_error("\n");
+		print_msg("error: cannot execute ");
+		print_msg(tmp[0]);
+		print_msg("\n");
 	}
 	exit(0);
 }
 
 int exec(char **cmd, char **env)
 {
-	int waitnbr = 0;
-	int in;
-	int pipefd[2];
-	pid_t pid;
+	int in = 0;
+	int wait_nbr = 0;
 	char **tmp = cmd;
+	pid_t pid;
+	int pipefd[2];
 
 	if (!next_pipe(cmd))
 		return (exec_cmd(cmd, env, tmp));
-	//? cmd with one or multiple pipe
-	// if ((in = dup(0)) < 0)
-		// return (error_fatal(cmd));
-	in = 0;
 	while (tmp)
 	{
 		if (pipe(pipefd) < 0 || (pid = fork()) < 0)
 			error_fatal(tmp);
 		if (pid == 0)
-			child_process(cmd, tmp, pipefd, in , env);
+			child_process(cmd, tmp, pipefd, in, env);
 		else
 		{
 			close(pipefd[0]);
 			close(pipefd[1]);
-			tmp = next_pipe(tmp);//free leaks
+			tmp = next_pipe(tmp);
 		}
-		waitnbr++;
+		wait_nbr++;
 	}
-	// close(in);
-	//? wait for child process to terminate
-	while (waitnbr-- > 0)
+	while (wait_nbr-- >= 0)
 		waitpid(0, NULL, 0);
 	return (0);
 }
 
 int main (int argc, char **argv, char **env)
 {
-	char **cmd;
 	int i;
+	char **cmd;
 
 	cmd = NULL;
 	i = 1;
@@ -184,7 +170,6 @@ int main (int argc, char **argv, char **env)
 		if (cmd)
 			free(cmd);
 		cmd = NULL;
-		i++;
 	}
 	return (0);
 }
